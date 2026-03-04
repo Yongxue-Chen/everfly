24e7a1e9915569dcc912903599dbbc44c6ea2bc8
unknown <chandler.yx.chen@gmail.com>
2026-03-04 13:04:19 +0000

    remove no need code; debug=false

--- app.py
+++ app.py
@@ -1761,7 +1761,4 @@ def update_missing_flights_aeroapi():
     return jsonify({'updated': updated_count, 'total_candidates': len(rows)})
 
 if __name__ == '__main__':
-    # Initialize DB (safe to run multiple times)
-    with app.app_context():
-        database.init_db()
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
